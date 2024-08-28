a5a9fb74eb9c68848210a344cf34d2bb9d4eb5a6
Vladislav Akashev <vlad.akashev.w@gmail.com>
2024-08-28 14:06:02 +0300

    Add mock data for vacation limits and schedule

---
+++ src/data/vacation_schedule.py
@@ -0,0 +1,5 @@
+vacation_schedule = [
+    {"start": "01.01.2025", "end": "15.01.2025", "type": "annual"},
+    {"start": "01.03.2025", "end": "05.03.2025", "type": "unpaid"},
+    {"start": "10.06.2025", "end": "20.06.2025", "type": "annual"}
+]
