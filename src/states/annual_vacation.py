d7aa522f33d4e5aa34def387eae9c89c5f1c5098
Vladislav Akashev <vlad.akashev.w@gmail.com>
2024-09-06 18:10:16 +0300

    Update annual_vacation_states and annual_vacation_transitions

--- src/states/annual_vacation.py
+++ src/states/annual_vacation.py
@@ -5,13 +5,6 @@ annual_vacation_states = [
     'annual_vacation_menu',
     'annual_vacation_confirm_period',  # Подтверждение выбранного периода отпуска
     'annual_vacation_create_vacation',
-
-    'annual_vacation_enter_start_date',
-    'annual_vacation_enter_end_date',
-    'annual_vacation_check_limits',
-    'annual_vacation_check_overlap',
-    'annual_vacation_create_vacation',
-    'annual_vacation_completed'
 ]
 
 # Переходы для состояний внутри annual_vacation
@@ -22,11 +15,4 @@ annual_vacation_transitions = [
      'dest': 'annual_vacation_create_vacation'},
     {'trigger': 'to_annual_vacation_create_vacation', 'source': 'annual_vacation_confirm_period',
      'dest': 'annual_vacation_create_vacation'},
-
-    {'trigger': 'enter_end_date', 'source': 'annual_vacation_enter_start_date', 'dest': 'annual_vacation_enter_end_date'},
-    {'trigger': 'check_limits', 'source': 'annual_vacation_enter_end_date', 'dest': 'annual_vacation_check_limits'},
-    {'trigger': 'check_overlap', 'source': 'annual_vacation_check_limits', 'dest': 'annual_vacation_check_overlap'},
-    {'trigger': 'create_vacation', 'source': 'annual_vacation_check_overlap', 'dest': 'annual_vacation_create_vacation'},
-    {'trigger': 'complete', 'source': 'annual_vacation_create_vacation', 'dest': 'annual_vacation_completed'},
-    {'trigger': 'finish_annual_vacation', 'source': 'annual_vacation_completed', 'dest': 'main_menu'}
 ]
