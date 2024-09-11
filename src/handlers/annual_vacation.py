0939c10801a336eb5b155fcf087fc8aa8f685d96
Vladislav Akashev <vlad.akashev.w@gmail.com>
2024-09-11 15:23:18 +0300

    Refactor annual vacation callback handlers - Updated vacation callbacks to handle confirmation and creation flows. - Added new state transitions for confirming annual vacations. - Refined logging and state management during vacation handling.

--- src/handlers/annual_vacation.py
+++ src/handlers/annual_vacation.py
@@ -1,188 +1,223 @@
 import json
 import logging
 from bot.event import Event, EventType
-from src.keyboards import (annual_vacation_buttons, confirm_period_keyboard,
+from src.actions import AnnualVacationActions as Actions
+
+from src.keyboards import (annual_vacation_buttons,
                            main_menu_keyboard, accept_period_keyboard)
-from src.states.state_machine import UserStateMachine
+from src.states import UserStateManager
 from src.models.vacation import Vacation, VacationType, VacationStatus
 from src.data.vacation_limits import vacation_limits_dict
 from src.data.vacation_schedule import vacation_schedule
-from src.utils import create_vacation_buttons  # Импортируем функцию из utils.py
+from src.utils import create_vacation_keyboard, create_keyboard, parse_callback_data, \
+    parse_vacation_dates
 
-# Настройка логгера для модуля
 logger = logging.getLogger(__name__)
 
-# Константы
-PLANNED_VACATION_CALLBACK = "planned_vacation"
-PRIMARY_STYLE = "primary"
-ANNUAL_VACATION_TEXT_TEMPLATE = (
+ANNUAL_VACATION_MENU_TEXT_TEMPLATE = (
     "Вывожу доступные плановые периоды. Можете выбрать один из них или выбрать другие даты. "
     "Доступно дней для оформления отпуска: {available_days}, но можно оформить и другое количество."
 )
-CONFIRM_PERIOD_TEXT_TEMPLATE = "Оформить отпуск на {period} или вы хотите немного изменить даты?"
+HANDLE_ANNUAL_VACATION_TEXT_TEMPLATE = "Оформить отпуск на {period} или вы хотите немного изменить даты?"
+CONFIRM_ANNUAL_VACATION_TEXT_TEMPLATE = "Оформление ежегодного отпуска {period} отправлено на согласование"
+CREATE_ANNUAL_VACATION_TEXT = "Введите период. Пожалуйста, укажите период в формате ДД.ММ.ГГГГ - ДД.ММ.ГГГГ"
+HANDLE_ANNUAL_VACATION_DATES_TEXT_TEMPLATE = "Вы точно хотите оформить отпуск на {period}?"
 
 
-def handle_annual_vacation(bot, state_machine, user_id: str, event: Event) -> None:
-    logger.info(f"Starting annual vacation process for user {user_id}")
+def annual_vacation_menu_cb(
+        bot,
+        user_state: UserStateManager,
+        user_id: str,
+        event: Event,
+        callback_data_value: str = None
+) -> None:
+    logger.info(f"Annual vacation menu callback for user {user_id}")
 
-    state_machine.to_annual_vacation_menu()
-    state_machine.save_state()
-    logger.info(f"State saved: {state_machine.state}")
+    user_state.state_machine.to_annual_vacation_menu()
+    user_state.save_state()
 
-    # Используем общую функцию для создания кнопок
-    planned_vacations_buttons = create_vacation_buttons(
+    annual_vacation_keyboard = create_vacation_keyboard(
         planned_vacations=vacation_schedule,
-        callback_prefix=PLANNED_VACATION_CALLBACK,
-        button_style=PRIMARY_STYLE
+        callback_prefix=Actions.HANDLE_ANNUAL_VACATION.value
     )
 
-    annual_vacation_menu_keyboard = planned_vacations_buttons + annual_vacation_buttons
+    actions = [
+        Actions.CREATE_ANNUAL_VACATION,
+        Actions.BACK_TO_MAIN_MENU
+    ]
+    annual_vacation_keyboard2 = create_keyboard(actions=actions)
+
+    annual_vacation_menu_keyboard = annual_vacation_keyboard + annual_vacation_keyboard2
     available_days = vacation_limits_dict[VacationType.ANNUAL_PAID].available_days
-    annual_vacation_text = ANNUAL_VACATION_TEXT_TEMPLATE.format(available_days=available_days)
+    annual_vacation_text = ANNUAL_VACATION_MENU_TEXT_TEMPLATE.format(available_days=available_days)
 
     bot.edit_text(
         chat_id=user_id,
-        msg_id=state_machine.last_message_id,
+        msg_id=user_state.get_last_bot_message_id(),
         text=annual_vacation_text,
         inline_keyboard_markup=json.dumps(annual_vacation_menu_keyboard)
     )
 
 
-def handle_planned_vacation(bot, state_machine, user_id: str, event: Event,
-                            vacation_dates: str) -> None:
-    """Обрабатывает выбор планового отпуска пользователем."""
-    logger.info(f"Handling planned vacation for user {user_id}")
+def handle_annual_vacation_cb(
+        bot,
+        user_state: UserStateManager,
+        user_id: str,
+        event: Event,
+        callback_data_value: str
+) -> None:
+    logger.info(f"Handle annual vacation callback for {user_id}")
 
-    state_machine.to_annual_vacation_confirm_period()
-    state_machine.set_vacation_dates(vacation_dates)
-    state_machine.save_state()
-    logger.info(f"State saved: {state_machine.state}")
+    user_state.state_machine.to_handle_annual_vacation()
+    user_state.set_current_vacation(vacation_id=callback_data_value)
+    user_state.save_state()
 
-    confirm_period_text = CONFIRM_PERIOD_TEXT_TEMPLATE.format(period=vacation_dates)
+    start_date, end_date = user_state.get_current_vacation_dates()
+    handle_annual_vacation_text = HANDLE_ANNUAL_VACATION_TEXT_TEMPLATE.format(period=f"{start_date}- {end_date}")
+
+    actions = [
+        Actions.CONFIRM_ANNUAL_VACATION,
+        Actions.CREATE_ANNUAL_VACATION,
+        Actions.BACK_TO_MAIN_MENU]
+    handle_annual_vacation_keyboard = create_keyboard(actions=actions)
+
     bot.edit_text(
         chat_id=user_id,
-        msg_id=state_machine.last_message_id,
-        text=confirm_period_text,
-        inline_keyboard_markup=json.dumps(confirm_period_keyboard)
+        msg_id=user_state.get_last_bot_message_id(),
+        text=handle_annual_vacation_text,
+        inline_keyboard_markup=json.dumps(handle_annual_vacation_keyboard)
     )
 
 
-def handle_confirm_vacation(bot, state_machine, user_id: str, event: Event) -> None:
-    """Обрабатывает подтверждение оформления отпуска пользователем."""
-    logger.info(f"Handling confirm vacation for user {user_id}")
+def create_annual_vacation_cb(
+        bot,
+        user_state: UserStateManager,
+        user_id: str,
+        event: Event,
+        callback_data_value: str
+) -> None:
+    logger.info(f"Create annual vacation callback for {user_id}")
+    user_state.state_machine.to_create_annual_vacation()
+    user_state.save_state()
 
-    state_machine.to_main_menu()
-    vacation_dates = state_machine.get_vacation_dates()
-    state_machine.reset_vacation_dates()
-    state_machine.save_state()
-    logger.info(f"State saved: {state_machine.state}")
+    bot.edit_text(
+        chat_id=user_id,
+        msg_id=user_state.get_last_bot_message_id(),
+        text=CREATE_ANNUAL_VACATION_TEXT
+    )
 
-    confirm_vacation_text = f"Оформление ежегодного отпуска {vacation_dates} отправлено на согласование"
+
+def confirm_annual_vacation_cb(
+        bot,
+        user_state: UserStateManager,
+        user_id: str,
+        event: Event,
+        callback_data_value: str
+) -> None:
+    logger.info(f"Confirm annual vacation callback for {user_id}")
+    # TODO: Добавить в будущем запрос для отправки отпуска на согласование
+    # current_vacation = user_state.get_current_vacation()
+
+    start_date, end_date = user_state.get_current_vacation_dates()
+    confirm_annual_vacation_text = CONFIRM_ANNUAL_VACATION_TEXT_TEMPLATE.format(period=f"{start_date} - {end_date}")
     bot.answer_callback_query(
         query_id=event.data['queryId'],
-        text=confirm_vacation_text,
+        text=confirm_annual_vacation_text,
         show_alert=False
     )
-
-    bot.edit_text(
-        chat_id=user_id,
-        msg_id=state_machine.last_message_id,
-        text="Главное меню",
-        inline_keyboard_markup=json.dumps(main_menu_keyboard)
-    )
+    back_to_main_menu_cb(bot, user_state, user_id, event, callback_data_value)
 
 
-def handle_create_vacation(bot, state_machine, user_id: str, event: Event) -> None:
-    """Обрабатывает создание нового отпуска пользователем."""
-    logger.info(f"Handling create new vacation for user {user_id}")
-
-    state_machine.to_annual_vacation_create_vacation()
-    state_machine.save_state()
-    logger.info(f"State saved: {state_machine.state}")
-
-    create_vacation_text = "Введите период. Пожалуйста, укажите период в формате ДД.ММ.ГГГГ - ДД.ММ.ГГГГ"
-
-    bot.edit_text(
-        chat_id=user_id,
-        msg_id=state_machine.last_message_id,
-        text=create_vacation_text
-    )
-
-
-def handle_accept_vacation(bot, state_machine, user_id: str, event: Event) -> None:
-    """Обрабатывает подтверждение пользователем оформления нового отпуска."""
-    logger.info(f"Handling accept vacation for user {user_id}")
-
-    vacation_dates = event.data['text']
-    state_machine.set_vacation_dates(vacation_dates)
-    state_machine.save_state()
-    accept_vacation_text = f"Вы точно хотите оформить отпуск на {vacation_dates}?"
-
-    bot.delete_messages(chat_id=user_id, msg_id=state_machine.last_message_id)
-    response = bot.send_text(
-        chat_id=user_id,
-        text=accept_vacation_text,
-        inline_keyboard_markup=json.dumps(accept_period_keyboard)
-    )
-    logger.info(f"Response: {response.json()}")
-    state_machine.last_message_id = response.json().get('msgId')
-    state_machine.save_state()
-
-
-def handle_back_to_main_menu(bot, state_machine, user_id: str, event: Event) -> None:
-    """Возвращает пользователя в главное меню."""
+def back_to_main_menu_cb(
+        bot,
+        user_state: UserStateManager,
+        user_id: str,
+        event: Event,
+        callback_data_value: str
+) -> None:
     logger.info(f"Handling back to main menu for user {user_id}")
 
-    state_machine.to_main_menu()
-    state_machine.reset_vacation_dates()
-    state_machine.save_state()
-    logger.info(f"State saved: {state_machine.state}")
+    user_state.reset_current_vacation_and_limit()
+    user_state.state_machine.to_main_menu()
+    user_state.save_state()
 
     bot.edit_text(
         chat_id=user_id,
-        msg_id=state_machine.last_message_id,
-        text="Главное меню",
+        msg_id=user_state.get_last_bot_message_id(),
+        text="Главное меню",  # TODO: Текст через actions
         inline_keyboard_markup=json.dumps(main_menu_keyboard)
+        # TODO: Сделать клавиатуру для главного меню через actions
     )
 
 
-annual_vacation_cb_handlers = {
-    PLANNED_VACATION_CALLBACK: handle_planned_vacation,
-    "confirm_planned_vacation": handle_confirm_vacation,
-    "create_new_vacation": handle_create_vacation,
-    "back_to_main_menu": handle_back_to_main_menu
+annual_vacation_callbacks = {
+    Actions.HANDLE_ANNUAL_VACATION.value: handle_annual_vacation_cb,
+    Actions.CREATE_ANNUAL_VACATION.value: create_annual_vacation_cb,
+    Actions.CONFIRM_ANNUAL_VACATION.value: confirm_annual_vacation_cb,
+    Actions.BACK_TO_MAIN_MENU: back_to_main_menu_cb
 }
 
 
+def annual_vacation_callback_dispatcher(bot, event: Event) -> None:
+    user_id = event.from_chat
+    user_state = UserStateManager.get_state(user_id)
+    callback_data = event.data.get('callbackData')
+    callback_data_prefix, callback_data_value = parse_callback_data(callback_data)
+    callback = annual_vacation_callbacks.get(callback_data_prefix)
+
+    if callback:
+        logger.info(f"Found callback for callback_data: {callback_data}")
+        callback(bot, user_state, user_id, event, callback_data_value)
+
+
+def create_annual_vacation_from_dates_cb(
+        bot,
+        user_state: UserStateManager,
+        user_id: str,
+        event: Event,
+) -> None:
+    logger.info(f" Accept annual vacation callback for {user_id}")
+    vacation_dates = event.data['text']
+    start_date, end_date = parse_vacation_dates(vacation_dates)
+    user_state.create_new_vacation(vacation_type=VacationType.ANNUAL_PAID,
+                                   start_date=start_date,
+                                   end_date=end_date)
+    user_state.state_machine.to_confirm_annual_vacation()
+    user_state.save_state()
+
+    actions = [
+        Actions.CONFIRM_ANNUAL_VACATION,
+        Actions.BACK_TO_MAIN_MENU]
+    handle_annual_vacation_keyboard = create_keyboard(actions=actions)
+
+    handle_annual_vacation_dates_text = HANDLE_ANNUAL_VACATION_DATES_TEXT_TEMPLATE.format(
+        period=f"{start_date} - {end_date}")
+
+    bot.delete_messages(chat_id=user_id, msg_id=user_state.get_last_bot_message_id())
+    response = bot.send_text(
+        chat_id=user_id,
+        text=handle_annual_vacation_dates_text,
+        inline_keyboard_markup=json.dumps(handle_annual_vacation_keyboard)
+    )
+    logger.info(f"Response: {response.json()}")
+    user_state.set_last_bot_message_id(response.json().get('msgId'))
+    # TODO: Может делать авто сохранение состояния?
+    user_state.save_state()
+
+
+# TODO: Как будто это стоит вынести в отедльный модуль. Для сообщений диспетчиризация идет по состоянию.
+#  Сделать одну фнукцию диспетчера для всех месседж колбежков?
 def annual_vacation_message_cb(bot, event: Event) -> None:
     """Обрабатывает входящие сообщения, связанные с ежегодными отпусками."""
     user_id = event.from_chat
-    state_machine = UserStateMachine.get_state(user_id)
-    logger.info(f"annual_vacation_message_cb for user: {user_id}, state: {state_machine.state}")
+    user_state = UserStateManager.get_state(user_id)
+    logger.info(f"annual_vacation_message_cb for user: {user_id}, state: {user_state.user_state}")
 
-    if state_machine.state != "annual_vacation_create_vacation":
-        logger.info(f"State mismatch: {state_machine.state} != annual_vacation_create_vacation")
+    # TODO: create_annual_vacation должно быть четко определено и возможно связано с actions
+    if user_state.user_state != "create_annual_vacation":
         return
 
     logger.info(f"Event type: {event.type}")
     if event.type == EventType.NEW_MESSAGE:
         logger.info(f"Handling new message event for user {user_id}")
-        handle_accept_vacation(bot, state_machine, user_id, event)
-
-
-def annual_vacation_cb(bot, event: Event) -> None:
-    """Обрабатывает обратные вызовы, связанные с ежегодными отпусками."""
-    user_id = event.from_chat
-    state_machine = UserStateMachine.get_state(user_id)
-    callback_data = event.data.get('callbackData')
-
-    if callback_data.startswith(PLANNED_VACATION_CALLBACK):
-        vacation_dates = callback_data[len(PLANNED_VACATION_CALLBACK) + 1:]  # +1 для учета "_"
-        handler = handle_planned_vacation
-
-        if handler:
-            handler(bot, state_machine, user_id, event, vacation_dates)
-    else:
-        handler = annual_vacation_cb_handlers.get(callback_data)
-        if handler:
-            handler(bot, state_machine, user_id, event)
+        create_annual_vacation_from_dates_cb(bot, user_state, user_id, event)
--- src/states/annual_vacation.py
+++ src/states/annual_vacation.py
@@ -1,18 +1,17 @@
-# Определение состояний и переходов для annual_vacation
-
-# Состояния, относящиеся к annual_vacation
 annual_vacation_states = [
     'annual_vacation_menu',
-    'annual_vacation_confirm_period',  # Подтверждение выбранного периода отпуска
-    'annual_vacation_create_vacation',
+    'handle_annual_vacation',
+    'create_annual_vacation',
+    'confirm_annual_vacation',
 ]
 
-# Переходы для состояний внутри annual_vacation
 annual_vacation_transitions = [
-    {'trigger': 'to_annual_vacation_confirm_period', 'source': 'annual_vacation_menu',
-     'dest': 'annual_vacation_confirm_period'},
-    {'trigger': 'to_annual_vacation_create_vacation', 'source': 'annual_vacation_menu',
-     'dest': 'annual_vacation_create_vacation'},
-    {'trigger': 'to_annual_vacation_create_vacation', 'source': 'annual_vacation_confirm_period',
-     'dest': 'annual_vacation_create_vacation'},
+    {'trigger': 'to_handle_annual_vacation', 'source': 'annual_vacation_menu',
+     'dest': 'handle_annual_vacation'},
+    {'trigger': 'to_create_annual_vacation', 'source': 'annual_vacation_menu',
+     'dest': 'create_annual_vacation'},
+    {'trigger': 'to_create_annual_vacation', 'source': 'handle_annual_vacation',
+     'dest': 'create_annual_vacation'},
+    {'trigger': 'to_confirm_annual_vacation', 'source': 'create_annual_vacation',
+     'dest': 'confirm_annual_vacation'},
 ]
