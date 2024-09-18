7a55fb0ab53a755dae0867a9f85100e0b8e1491b
Vladislav Akashev <vlad.akashev.w@gmail.com>
2024-09-18 11:25:35 +0300

    refactor: update Redis user data storage and retrieval methods
    - Refactored `save_user_data` to store user data as a JSON string in Redis using `model_dump_json`.
    - Simplified `get_user_data` to retrieve user data from Redis and handle both bytes and strings.
    - Added comments clarifying the behavior when `decode_responses=True` is used with Redis client.

--- src/services/redis_service.py
+++ src/services/redis_service.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Optional
 
 import redis
@@ -5,6 +6,8 @@ from redis.client import StrictRedis
 
 from config import REDIS_HOST, REDIS_PORT, REDIS_DB
 from models.user_data import UserData
+
+logger = logging.getLogger(__name__)
 
 
 class UserDataRedisService:
@@ -29,7 +32,8 @@ class UserDataRedisService:
         Args:
             user_data (UserData): The user data to save or update.
         """
-        self.redis_client.hset(user_data.user_id, mapping=user_data.model_dump())
+        user_data_json = user_data.model_dump_json(exclude_none=True)
+        self.redis_client.set(user_data.user_id, user_data_json)
 
     def get_user_data(self, user_id: str) -> Optional[UserData]:
         """
@@ -41,9 +45,12 @@ class UserDataRedisService:
         Returns:
             Optional[UserData]: The retrieved user data, or None if not found.
         """
-        data = self.redis_client.hgetall(user_id)
-        if data:
-            return UserData(**{key.decode('utf-8'): value.decode('utf-8') for key, value in data.items()})
+        raw_user_data = self.redis_client.get(user_id)
+        if raw_user_data:
+            # If you are using the option decode_responses=True when creating the Redis client,
+            # Redis will automatically return strings instead of bytes, and no decoding will be required.
+            user_data_json = raw_user_data.decode('utf-8') if isinstance(raw_user_data, bytes) else raw_user_data
+            return UserData.model_validate_json(user_data_json)
 
     def delete_user_data(self, user_id: str) -> None:
         """
