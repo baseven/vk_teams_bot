043b54396ac897d2c9afc1e0c9fafc06ccf89f87
Vladislav Akashev <vlad.akashev.w@gmail.com>
2024-10-17 11:42:51 +0300

    fix: Update annual vacation callback

--- src/callbacks/annual_vacation/bot_button_callbacks/confirm_vacation.py
+++ src/callbacks/annual_vacation/bot_button_callbacks/confirm_vacation.py
@@ -22,9 +22,27 @@ def confirm_annual_vacation_cb(
     user_session.state_machine.to_confirm_annual_vacation()
     user_session.save_session()
 
-    start_date, end_date = user_session.vacation_manager.get_current_vacation_dates()
-    vacation_period = format_vacation_period(start_date=start_date, end_date=end_date)
-    message_text = messages.annual_vacation.confirm_annual_vacation.format(period=vacation_period)
+    current_vacation_dates = user_session.vacation_manager.get_current_vacation_dates()
+    new_vacation_dates = user_session.vacation_manager.get_new_vacation_dates()
+
+    if current_vacation_dates and not new_vacation_dates:
+        vacation_period = format_vacation_period(start_date=current_vacation_dates[0],
+                                                 end_date=current_vacation_dates[1])
+        message_text = messages.annual_vacation.confirm_annual_vacation.format(period=vacation_period)
+
+    elif new_vacation_dates and not current_vacation_dates:
+        vacation_period = format_vacation_period(start_date=new_vacation_dates[0],
+                                                 end_date=new_vacation_dates[1])
+        message_text = messages.annual_vacation.confirm_annual_vacation.format(period=vacation_period)
+
+    else:
+        old_vacation_period = format_vacation_period(start_date=current_vacation_dates[0],
+                                                     end_date=current_vacation_dates[1])
+        new_vacation_period = format_vacation_period(start_date=new_vacation_dates[0],
+                                                     end_date=new_vacation_dates[1])
+        message_text = messages.annual_vacation.confirm_annual_vacation_reschedule.format(
+            old_period=old_vacation_period,
+            new_period=new_vacation_period)
 
     bot.answer_callback_query(
         query_id=event.data['queryId'],
